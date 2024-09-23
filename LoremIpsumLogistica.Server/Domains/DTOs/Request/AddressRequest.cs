deb2057581da4f7c73e86d1cc44000cb5700788d
andre-moura <andre.moura.tech@outlook.com>
2024-09-23 13:09:28 -0300

    feat: adicionando atributos de validação no DTO

--- LoremIpsumLogistica.Server/Domains/DTOs/Request/AddressRequest.cs
+++ LoremIpsumLogistica.Server/Domains/DTOs/Request/AddressRequest.cs
@@ -1,4 +1,5 @@
 ﻿using LoremIpsumLogistica.Server.Utils.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace LoremIpsumLogistica.Server.Domains.DTOs.Request
 {
@@ -6,20 +7,27 @@ namespace LoremIpsumLogistica.Server.Domains.DTOs.Request
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "O CEP é obrigatório.")]
         public string ZipCode { get; set; }
 
+        [Required(ErrorMessage = "O logradouro é obrigatório.")]
         public string Street { get; set; }
 
+        [Required(ErrorMessage = "O número é obrigatório.")]
         public string Number { get; set; }
 
         public string? Complement { get; set; }
 
         public string? Neighborhood { get; set; }
 
+        [Required(ErrorMessage = "A cidade é obrigatória.")]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "A UF é obrigatória.")]
+        [StringLength(2, ErrorMessage = "A UF deve ter exatamente 2 caracteres.")]
         public string State { get; set; }
 
+        [Required(ErrorMessage = "O tipo de endereço é obrigatório.")]
         public EAddressType AddressType { get; set; }
     }
 }
--- LoremIpsumLogistica.Server/Domains/DTOs/Request/CustomerRequest.cs.cs
+++ LoremIpsumLogistica.Server/Domains/DTOs/Request/CustomerRequest.cs.cs
@@ -1,13 +1,18 @@
 ﻿using LoremIpsumLogistica.Server.Utils.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace LoremIpsumLogistica.Server.Domains.DTOs.Request
 {
     public class CustomerRequest
     {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
         public DateTime DateOfBirth { get; set; }
 
+        [Required(ErrorMessage = "O gênero é obrigatório.")]
         public EGender Gender { get; set; }
 
         public ICollection<AddressRequest>? Addresses { get; set; }
